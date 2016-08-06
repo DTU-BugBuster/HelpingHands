8c1027dc2cd18439e8bbb163dd33fc9357ac7ee8
Aloy Gupta <aloygupta1993@gmail.com>
2016-08-06 18:32:03 +0530

    Changed HeroBannerFragment layout

--- app/src/main/java/com/helpinghands/activity/HomeActivity.java
+++ app/src/main/java/com/helpinghands/activity/HomeActivity.java
@@ -1,13 +1,40 @@
 package com.helpinghands.activity;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 
-public class HomeActivity extends Activity {
+import com.helpinghands.fragment.HeroBannerFragment;
+import com.helpinghands.fragment.NominateSMSContactsFragment;
+
+public class HomeActivity extends FragmentActivity implements
+        HeroBannerFragment.OnFragmentInteractionListener,
+        NominateSMSContactsFragment.OnFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        HeroBannerFragment nsmsc=new HeroBannerFragment();
+        pushFragment(nsmsc);
+    }
+
+
+
+    public void pushFragment(Fragment fragment){
+
+        FragmentTransaction ft=getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fragment_container,fragment);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
